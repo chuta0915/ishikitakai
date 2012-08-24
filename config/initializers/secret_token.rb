5376dec43dfcbbdca50c41837519eec88f6105ed
koshikawa <koshikawa@ppworks.jp>
2012-08-25 06:44:12 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IshikitakaiCom::Application.config.secret_token = '0a57f397bcceeb286c1e068726657108b4b1cc0011f5e865cf6f5ed1cb308900776b3665790806c382327af7cc73e29a3ff6915a8c61d3d4c136c9a5dd0e2a92'
