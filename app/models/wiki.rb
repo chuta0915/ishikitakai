c4a3e731933d945b6f339f707b0f6b7afa393f66
koshikawa <koshikawa@ppworks.jp>
2012-08-27 14:15:15 +0900

    Add paper_trail to Wiki model

--- app/models/wiki.rb
+++ app/models/wiki.rb
@@ -1,5 +1,6 @@
 class Wiki < ActiveRecord::Base
   include Common::Markdown
+  has_paper_trail
   attr_accessible :content, :name, :parent_id, :parent_type, :user_id
   belongs_to :parent, :polymorphic => true
 end
--- spec/models/wiki_spec.rb
+++ spec/models/wiki_spec.rb
@@ -6,23 +6,49 @@ describe Wiki do
   let(:event) { FactoryGirl.create :mokmok_event, user: user }
   describe 'Group wiki' do
     describe 'create wiki' do
-      subject { sendagayarb }
-      before do
-        subject.wikis.build content: 'wiki content'
+      context 'first' do
+        subject { sendagayarb }
+        before do
+          subject.wikis.create content: 'wiki content'
+        end
+        it { subject.should be_persisted }
+        it { subject.wikis[0].parent_type.should == 'Group' }
+        it { subject.wikis[0].versions.should have(1).items }
       end
-      it { subject.should be_valid }
-      it { subject.wikis[0].parent_type.should == 'Group' }
+      context 'second' do
+        subject { sendagayarb }
+        before do
+          subject.wikis.create content: 'wiki content1'
+          wiki = subject.wikis[0]
+          wiki.content = 'wiki content2'
+          wiki.save
+        end
+        it { subject.wikis[0].versions.should have(2).items }
+      end
     end
   end
 
   describe 'Event wiki' do
     describe 'create wiki' do
-      subject { event }
-      before do
-        subject.wikis.build content: 'wiki content'
+      context 'first' do
+        subject { event }
+        before do
+          subject.wikis.create content: 'wiki content'
+        end
+        it { subject.should be_persisted }
+        it { subject.wikis[0].parent_type.should == 'Event' }
+        it { subject.wikis[0].versions.should have(1).items }
       end
-      it { subject.should be_valid }
-      it { subject.wikis[0].parent_type.should == 'Event' }
+      context 'second' do
+        subject { sendagayarb }
+        before do
+          subject.wikis.create content: 'wiki content1'
+          wiki = subject.wikis[0]
+          wiki.content = 'wiki content2'
+          wiki.save
+        end
+        it { subject.wikis[0].versions.should have(2).items }
+      end
     end
   end
 end
