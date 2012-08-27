88704cda5eff9872a20c59c574baf873205e1713
koshikawa <koshikawa@ppworks.jp>
2012-08-27 14:06:18 +0900

    Add wiki model spec

--- spec/models/wiki_spec.rb
+++ spec/models/wiki_spec.rb
@@ -1,5 +1,28 @@
 require 'spec_helper'
 
 describe Wiki do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { FactoryGirl.create :user }
+  let(:sendagayarb) { FactoryGirl.create :sendagayarb, :user_id => FactoryGirl.create(:user).id }
+  let(:event) { FactoryGirl.create :mokmok_event, user: user }
+  describe 'Group wiki' do
+    describe 'create wiki' do
+      subject { sendagayarb }
+      before do
+        subject.wikis.build content: 'wiki content'
+      end
+      it { subject.should be_valid }
+      it { subject.wikis[0].parent_type.should == 'Group' }
+    end
+  end
+
+  describe 'Event wiki' do
+    describe 'create wiki' do
+      subject { event }
+      before do
+        subject.wikis.build content: 'wiki content'
+      end
+      it { subject.should be_valid }
+      it { subject.wikis[0].parent_type.should == 'Event' }
+    end
+  end
 end
