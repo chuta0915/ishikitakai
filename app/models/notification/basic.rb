433bd88c651614f08dddf39217cc154b56ef2b44
koshikawa <koshikawa@ppworks.jp>
2012-09-10 03:03:39 +0900

    Add Notification::Basic feature

--- app/models/notification/basic.rb
+++ app/models/notification/basic.rb
@@ -1,2 +1,24 @@
 class Notification::Basic < Notification
+  after_find :convert_for_locale
+
+  class << self
+    def notify_by_key users, key
+      params = {
+        name: "notification.basic.#{key}.name",
+        content: "notification.basic.#{key}.content",
+      }
+      self.notify users, params, nil
+    end
+  end
+
+  def read_it
+    self.reload
+    super
+  end
+
+  private
+  def convert_for_locale
+    self.name = I18n.t(self.name)
+    self.content = I18n.t(self.attributes['content'])
+  end
 end
---
+++ spec/models/notification/basic_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe Notification::Basic do
+  let(:user) { FactoryGirl.create :user }
+
+  describe "notify_by_key" do
+    subject { user.notifications.last }
+    before do
+      I18n.locale = :en
+      Notification::Basic.notify_by_key [user], 'confirm_email' 
+    end
+    it { subject.trigger_type.should == nil }
+    it { subject.trigger_id.should == nil }
+    it { subject.name.should =~ /email/ }
+    it { subject.content.should =~ /email/ }
+    it { subject.read.should be_false }
+    it { subject.read_at.should be_nil }
+  end
+end
