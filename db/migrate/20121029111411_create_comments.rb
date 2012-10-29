9700c26a53218ec4ee2f62b008b6d233ac33c36f
koshikawa <koshikawa@ppworks.jp>
2012-10-29 20:50:31 +0900

    Add index to comments table

--- db/migrate/20121029111411_create_comments.rb
+++ db/migrate/20121029111411_create_comments.rb
@@ -1,12 +1,15 @@
 class CreateComments < ActiveRecord::Migration
   def change
     create_table :comments do |t|
-      t.string :commentable_type
-      t.integer :commentable_id
-      t.integer :user_id
+      t.string :commentable_type, :null => false
+      t.integer :commentable_id, :null => false
+      t.integer :user_id, :null => false
       t.text :content
 
       t.timestamps
     end
+
+    add_index :comments, [:commentable_type, :commentable_id], :name => :idx_commentable_on_comments
+    add_index :comments, [:user_id], :name => :idx_user_id_on_comments
   end
 end
