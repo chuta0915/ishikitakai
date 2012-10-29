ab384ce9e82d2eb2791deb2bfc8614514928cd14
koshikawa <koshikawa@ppworks.jp>
2012-10-29 20:15:46 +0900

    Add Comment model
    rails g model comment commentable_type:string commentable_id:integer user_id:integer content:text

---
+++ spec/factories/comments.rb
@@ -0,0 +1,10 @@
+# Read about factories at https://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :comment do
+    commentable_type "MyString"
+    commentable_id 1
+    user_id 1
+    content "MyText"
+  end
+end
