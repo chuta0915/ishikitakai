3f92c5188cdd790277b386b5ffee09de65424b95
koshikawa <koshikawa@ppworks.jp>
2012-11-06 01:35:04 +0900

    Fix comments order bug

--- app/models/polymorphic/commentable.rb
+++ app/models/polymorphic/commentable.rb
@@ -1,6 +1,6 @@
 module Polymorphic::Commentable
   def self.included(base)
     base.belongs_to :user
-    base.has_many :comments, :as => :commentable
+    base.has_many :comments, :as => :commentable, :order => 'id ASC'
   end
 end
