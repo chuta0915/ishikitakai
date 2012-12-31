ef80db8eb5a02bc07d45b0338beaa34c2ec0ab5e
koshikawa <koshikawa@ppworks.jp>
2012-12-31 17:12:57 +0900

    [#41783225] User could change event's attendance mail setting

--- app/models/user_setting.rb
+++ app/models/user_setting.rb
@@ -1,3 +1,3 @@
 class UserSetting < ActiveRecord::Base
-  attr_accessible :mail_attend_status, :mail_event_comment, :user_id
+  attr_accessible :mail_attend_status, :mail_event_comment, :mail_event_attendance, :user_id
 end
