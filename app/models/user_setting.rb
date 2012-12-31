25b96912e6776bfdd25f302d9770e6d544c561e8
koshikawa <koshikawa@ppworks.jp>
2012-12-31 17:23:36 +0900

    [#41783219] User could change group's event mail setting

--- app/models/user_setting.rb
+++ app/models/user_setting.rb
@@ -1,3 +1,4 @@
 class UserSetting < ActiveRecord::Base
-  attr_accessible :mail_attend_status, :mail_event_comment, :mail_event_attendance, :user_id
+  attr_accessible :mail_attend_status, :mail_event_comment, :mail_event_attendance,
+   :mail_group_event, :user_id
 end
