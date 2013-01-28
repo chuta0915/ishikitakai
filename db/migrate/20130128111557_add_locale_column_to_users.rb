14d33d2342f1154e0ea72a813df93479f3c34ad4
koshikawa <koshikawa@ppworks.jp>
2013-01-28 20:31:07 +0900

    [#42468227] Update User#locale

--- db/migrate/20130128111557_add_locale_column_to_users.rb
+++ db/migrate/20130128111557_add_locale_column_to_users.rb
@@ -1,5 +1,10 @@
 class AddLocaleColumnToUsers < ActiveRecord::Migration
-  def change
+  def up
     add_column :users, :locale, :string
+    User.update_all(locale: 'ja')
+  end
+  
+  def down
+    remove_column :users, :locale
   end
 end
