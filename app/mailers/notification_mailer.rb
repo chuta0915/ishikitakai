ad578eb8bfcf9fb0f6477211b4e32d8b30a9c063
koshikawa <koshikawa@ppworks.jp>
2013-01-28 20:40:43 +0900

    [#42468227] Set locale before sending mail

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -5,25 +5,34 @@ class NotificationMailer < ActionMailer::Base
   def attend_status(user, event)
     @user = user
     @event = event
+    set_locale
     mail(:to => "#{user.name} <#{user.email}>", :subject => I18n.t("notification.attend_status.change_status.name", event_name: @event.name))
   end
 
   def event_comment(user, event)
     @user = user
     @event = event
+    set_locale
     mail(:to => "#{user.name} <#{user.email}>", :subject => I18n.t("notification.event_comment.post.name", event_name: @event.name))
   end
 
   def event_attendance(user, event)
     @user = user
     @event = event
+    set_locale
     mail(:to => "#{user.name} <#{user.email}>", :subject => I18n.t("notification.event_attendance.add.name", event_name: @event.name))
   end
 
   def group_event(user, event)
     @user = user
     @event = event
+    set_locale
     mail(:to => "#{user.name} <#{user.email}>", :subject => I18n.t("notification.group_event.add.name", group_name: @event.try(:group).try(:name)))
+  end
+
+  private
+  def set_locale
+    I18n.locale = @user.locale if @user.locale
   end
 end
 
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,6 +3,12 @@ class UserMailer < ActionMailer::Base
 
   def email_confirmation(user)
     @user = user
+    set_locale
     mail(:to => "#{user.name} <#{user.unconfirmed_email}>", :subject => "Confirm your email address")
+  end
+
+  private
+  def set_locale
+    I18n.locale = @user.locale if @user.locale
   end
 end
