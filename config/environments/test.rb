60cd42c2018f7c296eb3734586756b5adeb13a0d
koshikawa <koshikawa@ppworks.jp>
2013-05-05 10:46:53 +0900

    [#49254127] Fix sign in feature

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -5,11 +5,7 @@ IshikitakaiCom::Application.configure do
   # test suite. You never need to work with it otherwise. Remember that
   # your test database is "scratch space" for the test suite and is wiped
   # and recreated between test runs. Don't rely on the data there!
-  if defined?(Spork) && Spork.using_spork?
-    config.cache_classes = false
-  else
-    config.cache_classes = true
-  end
+  config.cache_classes = true
 
   # Configure static asset server for tests with Cache-Control for performance
   config.serve_static_assets = true
