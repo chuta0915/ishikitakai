d93a16e6a505a68ac7d7f561ea366a5776ca791c
koshikawa <koshikawa@ppworks.jp>
2013-05-05 10:31:27 +0900

    [#49254127] Fix the logic showing new event button

--- spec/features/events_spec.rb
+++ spec/features/events_spec.rb
@@ -2,9 +2,11 @@
 require 'spec_helper'
 
 describe 'Users' do
+  let(:user) { create(:user) }
   before do
     I18n.locale = :ja
   end
+
   describe 'visit events list' do
     let!(:event) { FactoryGirl.create(:mokmok_event) }
     before do
@@ -17,6 +19,26 @@ describe 'Users' do
 
     it '登録されているイベントが表示される' do
       page.should have_content(event.name)
+    end
+    
+    describe 'イベント作成ボタン' do
+      before do
+        Event.should_receive(:creatable?).and_return(creatable)
+        reload
+      end
+      context '作成出来る場合' do
+        let(:creatable) { true }
+        it '新規作成ボタンが表示される' do
+          page.should have_selector('a.new_event')
+        end
+      end
+
+      context '作成出来ない場合' do
+        let(:creatable) { false }
+        it '新規作成ボタンが表示されない' do
+          page.should_not have_selector('a.new_event')
+        end
+      end
     end
   end
 end
