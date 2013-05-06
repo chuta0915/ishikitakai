7be3426c0847e7eb89abc95acee6dcfa623430d3
koshikawa <koshikawa@ppworks.jp>
2013-05-06 17:42:16 +0900

    [#49254127] Move let to top

--- spec/features/events_spec.rb
+++ spec/features/events_spec.rb
@@ -3,17 +3,17 @@ require 'spec_helper'
 
 describe 'Events' do
   let(:user) { create(:user) }
+  let!(:public_group) { create(:sendagayarb, user_id: user.id) }
+  let!(:private_group) { create(:ishikitakai, user_id: user.id) }
+  let!(:public_event) { create(:mokmok_event) }
+  let!(:private_event) { create(:private_event) }
+  let!(:public_group_event) { create(:reading_event, group: public_group) }
+  let!(:private_group_event) { create(:drinkup_event, group: private_group) }
   before do
     I18n.locale = :ja
   end
 
   describe 'イベント一覧' do
-    let!(:public_group) { create(:sendagayarb, user_id: user.id) }
-    let!(:private_group) { create(:ishikitakai, user_id: user.id) }
-    let!(:public_event) { create(:mokmok_event) }
-    let!(:private_event) { create(:private_event) }
-    let!(:public_group_event) { create(:reading_event, group: public_group) }
-    let!(:private_group_event) { create(:drinkup_event, group: private_group) }
     before do
       visit events_path
     end
